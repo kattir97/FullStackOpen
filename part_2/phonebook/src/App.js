c338ee365c4d384b40bd6763e4760684b4b665ff
unknown <kemokatt@gmail.com>
2022-11-05 14:40:26 +0300

    complete phonebook excercise part 2

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -4,12 +4,15 @@ import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 import { Persons } from "./components/Persons";
 import personService from "./services/persons";
+import "./index.css";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [query, setQuery] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, SetNewNumber] = useState("");
+  const [message, setMessage] = useState(null);
+  const [messageClass, setMessageClass] = useState(null);
 
   useEffect(() => {
     const loadData = async () => {
@@ -24,39 +27,65 @@ const App = () => {
     SetNewNumber("");
   };
 
+  const displayMessage = () => {
+    if (!message) {
+      return null;
+    }
+
+    setTimeout(() => setMessage(false), 3000);
+
+    return <h3>{message}</h3>;
+  };
+
   const addNote = async (e) => {
     e.preventDefault();
 
-    let nameMatchObj = persons.find(
-      (obj) => obj.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
-    );
+    try {
+      let nameMatchObj = persons.find(
+        (obj) => obj.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
+      );
 
-    let numberMatchObj = persons.find((obj) => obj.number === newNumber);
+      let numberMatchObj = persons.find((obj) => obj.number === newNumber);
 
-    const newObj = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    };
+      const newObj = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
 
-    if (nameMatchObj && numberMatchObj) {
-      alert(`${newName} already is already added to the phonebook`);
-    } else if (nameMatchObj && !numberMatchObj) {
-      const updatedPerson = await personService.update(nameMatchObj.id, nameMatchObj, newNumber);
-      setPersons(persons.map((per) => (per.id !== updatedPerson.id ? per : updatedPerson)));
-      emptyFields();
-    } else {
-      const data = await personService.create(newObj);
-      setPersons([...persons, data]);
-      emptyFields();
+      if (nameMatchObj && numberMatchObj) {
+        alert(`${newName} already is already added to the phonebook`);
+      } else if (nameMatchObj && !numberMatchObj) {
+        const updatedPerson = await personService.update(nameMatchObj.id, nameMatchObj, newNumber);
+        setPersons(persons.map((per) => (per.id !== updatedPerson.id ? per : updatedPerson)));
+        setMessage("Number is updated");
+        displayMessage();
+        emptyFields();
+      } else {
+        const data = await personService.create(newObj);
+        setPersons([...persons, data]);
+        setMessage(`${newObj.name} is added`);
+        setMessageClass("message");
+        displayMessage();
+        emptyFields();
+      }
+    } catch (error) {
+      setMessage(`This person is already deleted`);
+      setMessageClass("delerr");
+      displayMessage();
     }
   };
 
   const handleDelete = async (id) => {
     if (window.confirm("You sure you want to delete?")) {
-      await personService.remove(id);
+      await personService.remove(id).catch((err) => {
+        return err;
+      });
       const arr = persons.filter((per) => per.id !== id);
       setPersons(arr);
+      setMessage("Number is deleted");
+      setMessageClass("delerr");
+      displayMessage();
       emptyFields();
     }
   };
@@ -84,6 +113,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div className="container">
+        {message ? (
+          <div className={messageClass === "delerr" ? "delerr" : "message"}>
+            {message ? displayMessage() : ""}
+          </div>
+        ) : null}
+      </div>
       <Filter onSearch={onSearch} />
       <br />
       <br />
